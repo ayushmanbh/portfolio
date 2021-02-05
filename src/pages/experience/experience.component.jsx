78b067931c47211369879ca7312a2142082c95cb
ayushmanbh <ashbh.dhp14@gmail.com>
2021-02-05 19:34:13 +0530

    added in experience section

--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
 import L_COGNIZANT from "../../assets/img/experience/cognizants-logo.svg";
+import L_BOT2DO from "../../assets/img/experience/bot2do.png";
 import Tilt from "react-tilt";
 import "./experience.styles.css";
 
@@ -15,7 +16,7 @@ const Experience = () => {
           <Tilt options={{ max: 25 }}>
             <Card>
               <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
-                <Card.Img variant="top" className="img-resize" src={L_COGNIZANT} alt="Accenture logo" />
+                <Card.Img variant="top" className="img-resize" src={L_COGNIZANT} alt="cognizant logo" />
               </Card.Header>
               <Card.Body className="d-flex justify-content-center flex-column">
                 <div>
@@ -25,19 +26,57 @@ const Experience = () => {
                   <div className="text-center style">
                     <strong className="body-title-style ">Enterprise Application Services - DCX</strong>
                     <br />
-                    <strong>Technology:</strong> Java, Node.js, AWS, Genesys
+                    <strong>Technology:</strong> Java, EcmaScript, Genesys
                     <br />
                     <strong>Duration:</strong> July 2020 - Present
                     <br />
                     <strong> Description </strong>
                     <ul className="text-left">
-                      <li><strong>Developed &amp; enhanced</strong> multiple features with customizability option across enterprise level application.</li>
-                      <li><strong>Developed</strong> Chatbot/IVR modules in contact center environment.
+                      <li><strong>Well Trained</strong> in Contact Center Technology</li>
+                      <li><strong>Developed</strong> small features in contact center environment using Java.
                       </li>
                       <li><strong>Provided</strong> assistance to senior colleagues according to changes in client specifications.
                       </li>
-                      <li><strong>Performed</strong> CRUD operations on multiple databases to load/ remove data according
-                      to the business requirements.</li>
+                      <li><strong>Created</strong> EcmaScript expressions to build routing logic for the contact center application.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Tilt>
+        </Container>
+      </Jumbotron>
+
+
+      <Jumbotron className="jumbo-style">
+        <Container>
+          <Tilt options={{ max: 25 }}>
+            <Card>
+              <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
+                <Card.Img variant="top" className="img-resize" src={L_BOT2DO} alt="bot2do logo" />
+              </Card.Header>
+              <Card.Body className="d-flex justify-content-center flex-column">
+                <div>
+                  <Card.Title className="text-center">Backend Developer</Card.Title>
+                </div>
+                <div>
+                  <div className="text-center style">
+                    <strong>Technology:</strong> Nodejs, Express, RestAPI, MySql, React
+                    <br />
+                    <strong>Duration:</strong> April 2020 - May 2020
+                    <br />
+                    <strong> Description </strong>
+                    <ul className="text-left">
+                      <li><strong>Developed</strong> Rest APIs using express framework amd MySql database.</li>
+                      <li><strong>Worked</strong> with the team of experienced developers to understand coding
+                        standards and architectural practices.
+                      </li>
+                      <li><strong>Provided</strong> assistance to Sr. Developers with tasks other than writing code like
+                        documenting Rest Api leveraging open source tools such as swagger ui.
+                      </li>
+                      <li><strong>Wrote</strong> CRUD operations in Node.js and Express framework to develop
+                        RESTful API which interacts with React client.</li>
                     </ul>
                   </div>
                 </div>
